fbb533451796bbde4edc68827661412dab6e259a
jarvis-byte-github <arkamazumder0@gmail.com>
2021-01-03 14:14:28 +0530

    First Initial Commit

---
+++ app/src/main/java/com/example/musicplayer/AlbumFragment.java
@@ -0,0 +1,26 @@
+package com.example.musicplayer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class AlbumFragment extends Fragment {
+
+
+    public AlbumFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_album, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/musicplayer/SongsFragment.java
@@ -0,0 +1,24 @@
+package com.example.musicplayer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SongsFragment extends Fragment {
+
+    public SongsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_songs, container, false);
+        return view;
+    }
+}
